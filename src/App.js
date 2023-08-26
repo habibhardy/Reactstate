13027481bf158c0f24e05026d4ba36f5a0835227
habibhardy <habibhardy10@gmail.com>
2023-08-26 04:24:01 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    person: {
+      fullName: "John Doe",
+      bio: "A brief description about me.",
+      imgSrc: "profile-image.jpg",
+      profession: "Software Developer",
+    },
+    show: false,
+  };
+
+
+  toggleProfile = () => {
+    this.setState(prevState => ({ show: !prevState.show }));
+  };
+
+  
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    return (
+      <div className="App">
+        <button onClick={this.toggleProfile}>Toggle Profile</button>
+        {this.state.show && (
+          <div className="profile">
+            <img src={imgSrc} alt={fullName} />
+            <h2>{fullName}</h2>
+            <p>{bio}</p>
+            <p>Profession: {profession}</p>
+          </div>
+        )}
+   
+      </div>
+    );
+  }
 }
 
 export default App;
